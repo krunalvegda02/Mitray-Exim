385fb7db255448abd45b973c7b18b9c94d3fb61a
stzkdigitalmedia <tanuthakue2@gmail.com>
2026-06-29 22:19:45 +0530

    ..

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const nextConfig = {
       { protocol: "https", hostname: "res.cloudinary.com" },
       { protocol: "https", hostname: "drive.google.com" }
     ],
+    unoptimized: true,
   },
 };
 
